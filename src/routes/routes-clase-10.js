16545c72e91c3f5e39851133a66a21b10bd72ddf
AntoniCut <antonicut@gmail.com>
2026-03-08 22:00:37 +0100

    Add: Clase 10. Manipulación avanzada del contenido

--- src/routes/routes-clase-10.js
+++ src/routes/routes-clase-10.js
@@ -57,9 +57,9 @@ export const routesClase10 = [
 
 
     {
-        id: 'clase10AppendPreppend',
+        id: 'clase10AppendPrepend',
         favicon: favicon,
-        pageTitle: "Clase 10 - 1. Inyección Append / Prepend",
+        pageTitle: "Clase 10 - 1. Inyección Append / Prepend / AppendTo / PrependTo",
         path: "/clase10-inyeccion-append-prepend",
         components: {
             "#layoutHeader": layoutHeader,
@@ -70,13 +70,13 @@ export const routesClase10 = [
             "#layoutMain": `${pages}/clase-10/01-inyeccion-append-prepend.html`,
             "#layoutFooter": layoutFooter,
         },
-        headerTitle: "Clase 10 - 1. Inyección Append / Prepend",
+        headerTitle: "Clase 10 - 1. Inyección Append / Prepend / AppendTo / PrependTo",
         styles: [
-            { href: `${styles}/pages/clase-10/01-inyeccion-append-preppend.css` },
+            { href: `${styles}/pages/clase-10/01-inyeccion-append-prepend.css` },
         ],
         scripts: [
             { src: `${scripts}/tooltips.js` },
-            { src: `${scripts}/clase-10/01-inyeccion-append-preppend.js` }
+            { src: `${scripts}/clase-10/01-inyeccion-append-prepend.js` }
         ]
     },
 
@@ -84,7 +84,7 @@ export const routesClase10 = [
     {
         id: 'clase10AfterBefore',
         favicon: favicon,
-        pageTitle: "Clase 10 - 2. Inyección After / Before",
+        pageTitle: "Clase 10 - 2. Inyección After / Before / InsertAfter / InsertBefore",
         path: "/clase10-inyeccion-after-before",
         components: {
             "#layoutHeader": layoutHeader,
@@ -95,7 +95,7 @@ export const routesClase10 = [
             "#layoutMain": `${pages}/clase-10/02-inyeccion-after-before.html`,
             "#layoutFooter": layoutFooter,
         },
-        headerTitle: "Clase 10 - 2. Inyección After / Before",
+        headerTitle: "Clase 10 - 2. Inyección After / Before / InsertAfter / InsertBefore",
         styles: [
             { href: `${styles}/pages/clase-10/02-inyeccion-after-before.css` },
         ],
--- src/scripts/clase-10/04-envoltura.js
+++ src/scripts/clase-10/04-envoltura.js
@@ -14,6 +14,21 @@
     console.log('\n');
 
 
+    //  -----  Envuelve el div con clase "envoltura" dentro de un article  -----
+    $('.envoltura')
+        .wrap("<article> </article>");
+
+
+    //  -----  Envuelve el article dentro de una section  -----
+    $('article')
+        .wrap("<section> </section>");
+
+
+    //  -----  desenvolvemos el texto del b, quitando las etiquetas li y ul  -----      
+    $('b')
+        .unwrap()
+        .unwrap();
+
 
 
 })(jQuery);
