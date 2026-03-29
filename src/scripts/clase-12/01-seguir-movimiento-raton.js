16eb75aee7c233bf212b2e3e4dfb67dadddee77c
AntoniCut <antonicut@gmail.com>
2026-03-29 18:43:33 +0200

    Add: - Clase 12. Eventos de teclado y ratón - Terminado - Añadir Explicación de cada ejercicio y sus estilos para el resto de los ejercicios del proyecto

--- src/scripts/clase-12/01-seguir-movimiento-raton.js
+++ src/scripts/clase-12/01-seguir-movimiento-raton.js
@@ -14,10 +14,27 @@
 
     //  -----  Referencias al HTML  -----
 
-    /** @type {JQuery<HTMLDivElement>} -  `Div para el método on()`  */
-    const $div = $(".div");
+    /** @type {JQuery<HTMLDivElement>} -  `Div con id = moverse`  */
+    const $moverse = $('#moverse');
 
+    //  -----  Verificación del elemento HTML  -----
+    if (!$moverse.length) 
+        return;
 
+    
+    //  -----  Permite hacer click en otros elementos aunque este div siga al cursor  -----
+    $moverse.css('pointer-events', 'none');
+    
+
+    //  -----  Seguir el movimiento del ratón  -----
+    $(document).on('mousemove', function (evento) {
+
+        $moverse.css({
+            top: evento.pageY + "px",
+            left: evento.pageX + "px",
+        });
+
+    });
     
 
 })(jQuery);
